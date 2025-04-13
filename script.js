dc6c73072afed4e3675d4eda566e8af197268acb
Kfkif <99655986+Kfkif@users.noreply.github.com>
2025-04-13 08:17:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function generateStory() {
+  const story = "एक समय की बात है, एक छोटे से गाँव में एक बच्चा रहता था जिसे कहानियाँ सुनना बहुत पसंद था...";
+  document.getElementById("story").innerText = story;
+}
